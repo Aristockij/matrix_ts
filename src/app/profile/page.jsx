7408d20156f3be1955314c821185b657e5507e5d
pepega <aristockij@gmail.com>
2025-08-29 07:56:52 +0300

    add profile dynamic

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import PersonInfo from "@/components/PersonInfo";
+export const dynamic = "force-dynamic";
 
 const page = () => {
   return (
